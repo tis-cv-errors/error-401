6d933b97f3c99d018dc633ddc5cb16ff52a1445d
Guillaume Millot <guillaume.millot@gmail.com>
2020-01-17 17:24:33 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+
+int main(void)
+{
+    int x = 0;
+    
+    y = my_func(x);
+    
+    return 0;
+}
+
+int my_func(int x) {
+    
+    assert(x > 0);
+    
+    return -x;
+    
+}
